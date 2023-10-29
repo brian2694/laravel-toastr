aaf1b933153b09d11eaf0c68a49c35a0aeb065c7
Antonio Bruno <300245+antonio8101@users.noreply.github.com>
2023-10-29 09:36:59 +0000

    set doc on the facade class

--- src/Facades/Toastr.php
+++ src/Facades/Toastr.php
@@ -4,6 +4,15 @@ namespace Brian2694\Toastr\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static message():string
+ * @method static add( $type, $message, $title = null, $options = [] )
+ * @method static info( $message, $title = null, $options = [] )
+ * @method static success( $message, $title = null, $options = [] )
+ * @method static warning( $message, $title = null, $options = [] )
+ * @method static error( $message, $title = null, $options = [] )
+ * @method static clear( $message, $title = null, $options = [] )
+ */
 class Toastr extends Facade
 {
     protected static function getFacadeAccessor()
