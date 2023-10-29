3d266f606670e15571ada7dd870d5b196f7026fc
Antonio Bruno <300245+antonio8101@users.noreply.github.com>
2023-10-29 09:41:51 +0000

    sets return on all class methods

--- src/Facades/Toastr.php
+++ src/Facades/Toastr.php
@@ -6,12 +6,12 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static message():string
- * @method static add( $type, $message, $title = null, $options = [] )
- * @method static info( $message, $title = null, $options = [] )
- * @method static success( $message, $title = null, $options = [] )
- * @method static warning( $message, $title = null, $options = [] )
- * @method static error( $message, $title = null, $options = [] )
- * @method static clear( $message, $title = null, $options = [] )
+ * @method static add( $type, $message, $title = null, $options = [] ):void
+ * @method static info( $message, $title = null, $options = [] ):void
+ * @method static success( $message, $title = null, $options = [] ):void
+ * @method static warning( $message, $title = null, $options = [] ):void
+ * @method static error( $message, $title = null, $options = [] ):void
+ * @method static clear( $message, $title = null, $options = [] ):void
  */
 class Toastr extends Facade
 {
